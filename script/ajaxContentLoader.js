9bfcbd0fd12c94f343a9d6fb3e1f356300316e38
halex02 <halex02@msn.com>
2015-12-11 23:39:13 +0100

    javascript : correction formatlink function

--- script/ajaxContentLoader.js
+++ script/ajaxContentLoader.js
@@ -1,7 +1,7 @@
 var init_save = $(".main-col-2").clone() ;
 var jLinks = $(".post-link") ;
 var i = 0 ;
-
+/*
 function getLink(){
   return $(this).attr("href") ;
 }
@@ -9,14 +9,14 @@ function getLink(){
 function formatLink(s){
   return s.concat(" #LoadableContent") ;
 }
-
+*/
 function ajaxRequestCompletion(){
   console.log("Requête complétée.") ;
 }
 
 function mouseenterHandler(){
     $(".main-col-2").empty() ;
-    $(".main-col-2").load(formatLink(getLink()), ajaxRequestCompletion) ;
+    $(".main-col-2").load($(this).attr("href").concat(" #LoadableContent"), ajaxRequestCompletion) ;
     console.log("DOM Element Entered") ;
 }
 
