3a042c7c2d381e7d10b27ed424778748bf5fb0b0
halex02 <halex02@msn.com>
2016-01-28 19:17:46 +0100

    push nouvelle page

---
+++ script/collection.js
@@ -0,0 +1,12 @@
+/**
+ * Liste des editions (objets DOM : span)
+ * Constante globale initialisé au chargement du script, ne pas modifier.
+ */
+var G_EDITIONS = $(".edition-name") ;
+
+/**
+ * Sauvegarde de l'état initial de la zone de chargement.
+ * Constante globale initialisé au chargement du script, ne pas modifier.
+ */
+var G_INITIAL_CONTENT_SAVE = $("#loadable-content").clone() ;
+
